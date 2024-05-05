66375f0d255c0b99c0f5995c9793e951f62ccf21
CarlesRojas <carlesrojas@outlook.com>
2024-05-05 11:51:03 +0200

    animated sprites working

--- script/AnimatedSprite.gd
+++ script/AnimatedSprite.gd
@@ -4,8 +4,9 @@ extends Node
 @onready var sprite_2d = $Sprite2D
 @onready var sprite_info = JSON.parse_string(FileAccess.get_file_as_string(sprite_manager.get_meta("AnimationJsonPath")))
 @onready var sprites = {}
-
 @onready var currentAnimation = "idle"
+@onready var basePosition = {}
+
 
 func _ready():
 	for part in sprite_info.parts:
@@ -15,8 +16,10 @@ func _ready():
 	positionSprites()
 	setAnimation(currentAnimation, true);
 
+
 func _process(delta):
 	update_animation()
+
 
 func createPartSprite(part, x, y, width, height):
 	var sprite = Sprite2D.new()
@@ -51,40 +54,49 @@ func positionSprites():
 		sprites[part].position.x = origin.x
 		sprites[part].position.y = origin.y
 		sprites[part].z_index = z_index
-		
-#      const { width, height } = this._info.parts[part].bounds;
-#      const { x: originX, y: originY } = this._info.parts[part].origin;
-#      const { x: anchorX, y: anchorY } = this._info.parts[part].anchor;
-#      const zIndex = this._info.parts[part].zIndex;
-#
-#      this._sprites[part].width = width * this._pixel * tile;
-#      this._sprites[part].height = height * this._pixel * tile;
-#      this._sprites[part].x = originX * this._pixel * tile;
-#      this._sprites[part].y = originY * this._pixel * tile;
-#      this._sprites[part].anchor.set(anchorX, anchorY);
-#      this._sprites[part].zIndex = zIndex;
-#    }
-#
-#    // Set initial positions
-#    this._basePosition = {};
-#
-#    for (const state in this._info.states) {
-#      this._basePosition[state] = [];
-#
-#      for (const animation of this._info.states[state].animations) {
-#        const { parts, type } = animation;
-#        const partsPositions: { [key: string]: number } = {};
-#
-#        for (const part of parts) {
-#          if (type === 'x') partsPositions[part] = this._sprites[part].x;
-#          else if (type === 'y') partsPositions[part] = this._sprites[part].y;
-#          else if (type === 'rotation') partsPositions[part] = 0;
-#        }
-#
-#        this._basePosition[state].push(partsPositions);
-#      }
-#    }
-#  }
+		sprites[part].offset.x = (bounds.width * 0.5) - anchor.x * bounds.width
+		sprites[part].offset.y = (bounds.height * 0.5) -anchor.y * bounds.height
+
+#    
+	basePosition = {};
+	for state in sprite_info.states:
+		basePosition[state] = [];
+		for animation in sprite_info.states[state].animations:
+			var type = animation.type
+			var parts = animation.parts
+			var partsPositions = {}
+
+			for part in parts:
+				if type == "x":
+					partsPositions[part] = sprites[part].position.x
+				elif type == "y":
+					partsPositions[part] = sprites[part].position.y
+				elif type == "rotation":
+					partsPositions[part] = 0
+
+			basePosition[state].append(partsPositions);
+
 
 func update_animation():
-	pass
+	var time = Time.get_ticks_msec()
+	
+	for state in sprite_info.states:
+		if state != currentAnimation: continue
+		var animations = sprite_info.states[state].animations
+
+		for i in range(len(animations)):
+			var animation = animations[i]
+			var currentSin = sin((time / 1000.0) * animation.speed);
+
+			for j in range(len(animation.parts)):
+				var part = animation.parts[j]
+				var invert = -1 if animation.inverted[j] else 1
+				var initial = basePosition[state][i][part];
+
+				if animation.type == "x":
+					sprites[part].position.x = initial + currentSin * animation.amplitude * invert
+				elif animation.type == "y":
+					sprites[part].position.y = initial + currentSin * animation.amplitude * invert
+				elif animation.type == "rotation":
+					sprites[part].rotation = currentSin * animation.amplitude * (PI / 180) * invert
+
